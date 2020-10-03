0f4257310ccfd6d0d3a56bf221902bded8dfd6f4
Nicolas Santos <nicolassantos16@gmail.com>
2020-10-03 19:19:30 -0300

    Fix arreglos con repeticion

--- lab1.pl
+++ lab1.pl
@@ -1,15 +1,15 @@
 % largo(+L, ?N)
 % Retorna el largo de la lista L
 largo([],0).
-largo([_|T],L1):-largo(T,L2),L1 is L2+1.
+largo([_|T],L1):- largo(T,L2),L1 is L2+1.
 
-% pertence(?X, ?L)
-% El elemento X pertence a la lista L
+% pertenece(?X, ?L)
+% El elemento X pertenece a la lista L
 pertenece(X,[X|_]).
 pertenece(X,[_|T]):- pertenece(X,T).
 
-% no_pertence(+X, +L)
-% El elemento X no pertence a la lista L
+% no_pertenece(+X, +L)
+% El elemento X no pertenece a la lista L
 no_pertenece(_,[]).
 no_pertenece(X,[Y]):-X\=Y.
 no_pertenece(X,[Y|T]):-X\=Y,no_pertenece(X,T).
@@ -20,12 +20,12 @@ elegir(X, [X|T], T).
 elegir(X, [Z|T], [Z|O]) :- elegir(X, T, O).
 
 % contenida(+L1, +L2)
-% Todos los elementos de L1 pertencen a L2
+% Todos los elementos de L1 pertenecen a L2
 contenida([], [_X|_Y]).
 contenida([X|T], W) :- pertenece(X, W), contenida(T, W).
 
 % sublista(+L, ?Sub)
-% Sub contiene un subconjunto de elementos contiguos de L en el mismo orden que aparecen en L
+% Sub contiene un subcopenjunto de elementos contiguos de L en el mismo orden que aparecen en L
 sublista(Z, X) :- prefijo(Z, X).
 sublista([_|T], X) :- prefijo(T, X).
 
@@ -41,8 +41,7 @@ arreglo(L, N, X) :- combinacion(L, N, W), permutacion(W, X).
 % arreglo_con_repeticion(+L1, +N, ?L2)
 % La lista L2 es un arreglo con repeticion de N elementos de la lista L1
 arreglo_con_repeticion(_, 0, []).
-arreglo_con_repeticion([H|T], N, [H|Tp]) :- N>0, Np is N-1, arreglo_con_repeticion([H|T], Np, Tp).
-arreglo_con_repeticion([_|T], N, X) :- N>0, arreglo_con_repeticion(T, N, X).
+arreglo_con_repeticion(L, N, [H|T]) :- N>0, Np is N-1, pertenece(H, L), arreglo_con_repeticion(L, Np, T).
 
 % combinacion(+L1, +N, -L2)
 % La lista L2 es una combinacion de N elementos de la lista L1.
