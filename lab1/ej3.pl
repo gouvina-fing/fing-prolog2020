da8216483ef7358b0aef5c6c55d63964c0d003da
gouvina <germanouvina@gmail.com>
2020-10-03 19:08:29 -0300

    estados y simbolos implementado

--- lab1/ej3.pl
+++ lab1/ej3.pl
@@ -15,14 +15,14 @@ es_afnd_eps(af(Q,S,F)) :- estados(Q), simbolos(S), finales(F,Q).
 % estados(Q)
 % Comprueba si la lista de estados Q contiene naturales sin repetir e incluye al 0
 estados([0]).
-estados([0|T]) : naturales_sin_cero(T).
-estados([H|T]) :- number(H), estados(T).
-
-naturales_sin_cero([H]) :- number(H).
-naturales_sin_cero([H|T]) :- number(H), naturales_sin_cero(T).
+estados([0|T]) :- no_pertenece(0,T), estados_sin_cero(T).
+estados([H|T]) :- number(H), H > 0, no_pertenece(H,T), estados(T).
+estados_sin_cero([H]) :- number(H), H > 0.
+estados_sin_cero([H|T]) :- number(H), H > 0, no_pertenece(H,T), estados_sin_cero(T).
 
 % simbolos(S)
-simbolos([H|T]).
+simbolos([H|[]]).
+simbolos([H|T]) :- no_pertenece(H,T), simbolos(T).
 
 % finales(F, Q)
 % Comprueba si la lista de estados F se encuentra contenida en la lista de estados Q
@@ -49,8 +49,3 @@ no_pertenece(X,[Y|T]):-X\=Y,no_pertenece(X,T).
 % Todos los elementos de L1 pertencen a L2
 contenida([], [_X|_Y]).
 contenida([X|T], W) :- pertenece(X, W), contenida(T, W).
-
-% sin_repetidos(+L1)
-% No hay elementos repetidos en L1
-sin_repetidos([]).
-sin_repetidos([H|T]) :- no_pertenece(H,T), sin_repetidos(T).
