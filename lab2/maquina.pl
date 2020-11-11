b42a2078cce86d34b99c8b99ff497a1330c357d6
Nicolas Santos <nicolassantos16@gmail.com>
2020-11-10 21:13:07 -0300

    Primer implementacion de maquina

--- lab2/maquina.pl
+++ lab2/maquina.pl
@@ -7,41 +7,97 @@
 :- use_module(core).
 :- use_module(utils).
 
-iniciar_maquina(X, Turno, Tablero, Score1, Score2, CasaElegida) :-
-  minimax(Turno, ).
+iniciar_maquina(Depth, Turno, Tablero, Score1, Score2, CasaElegida) :-
+  minimax(Depth, Turno, Tablero, Score1, Score2, CasaElegida, _HeuristicValue).
 
-minimax(jugador1, Tablero, Score1, Score2, CasaElegida) :-
-    separar_tablero(Tablero, Casas1, _),
-    casas_viables(jugador1, Casas1, CasasViables),
-    mejor_valor(jugador1, Tablero, Score1, Score2, CasasViables, CasaElegida, Valor), !
+% Paso base
+minimax(0, _Turno, _Tablero, Score1, Score2, _CasaElegida, HeuristicValue) :-
+  heuristic_value(Score1, Score2, HeuristicValue).
+minimax(_Depth, _Turno, _Tablero, Score1, Score2, _CasaElegida, HeuristicValue) :-
+  terminal_node(Score1, Score2),
+  heuristic_value(Score1, Score2, HeuristicValue).
+
+% Paso Inductivo
+minimax(Depth, Turno, Tablero, Score1, Score2, CasaElegida, HeuristicValue) :-
+  posibles_casas(Turno, Casas),
+  mejor_casa(Casas, Depth, Turno, Tablero, Score1, Score2, CasaElegida, HeuristicValue).
+
+% Paso base para elegir la mejor casa
+mejor_casa([ Casa ], Depth, Turno, Tablero, Score1, Score2, Casa, HeuristicValue) :-
+  (
+    realizar_movimiento(Casa, Tablero, Turno, Score1, Score2, NuevoScore1, NuevoScore2, NuevoTablero) ->
+    (
+      realizar_movimiento(Casa, Tablero, Turno, Score1, Score2, NuevoScore1, NuevoScore2, NuevoTablero),
+      NewDepth is Depth - 1,
+      cambiar_turno(Turno, NuevoTurno),
+      minimax(NewDepth, NuevoTurno, NuevoTablero, NuevoScore1, NuevoScore2, Casa, HeuristicValue), !
+    )
     ;
-    evaluar(Score1, Score2, Valor).
+    (
+      devolver_peor_val_heuristica(Turno, HeuristicValue)
+    ) 
+  ).
 
-
-mejor_valor(Jugador, Tablero, Score1, Score2, [CasaElegida], CasaElegida, Valor) :-
-    
-    minimax(Jugador, CasaElegida, _, Valor), !.
-mejor_valor(Jugador, [Casa1 | CasasViables], Casa, Valor) :-
-    minimax(Jugador, Casa1, _, Valor1),
-    mejor_valor(Jugador, CasasViables, Casa2, Valor2),
-    mejor_entre(Jugador, Casa1, Valor1, Casa2, Valor2, Casa, Valor).
-
-mejor_entre(jugador1, Casa1, Valor1, _, Valor2, Casa1, Valor1) :-  
-    Valor1 > Valor2, !.
-mejor_entre(jugador2, Casa1, Valor1, _, Valor2, Casa1, Valor1) :- 
-    Valor1 < Valor2, !.
-mejor_entre(_, _, _, Casa2, Valor2, Casa2, Valor2).  
-
-evaluar(Score1, Score2, Valor) :-
-    Score1 > Score2
+mejor_casa([ Casa| CasasRestantes ], Depth, Turno, Tablero, Score1, Score2, CasaElegida, HeuristicValue) :-
+  (
+    realizar_movimiento(Casa, Tablero, Turno, Score1, Score2, NuevoScore1, NuevoScore2, NuevoTablero)
     ->
-    Valor is Score1 - Score2
+    (
+      NewDepth is Depth - 1,
+      cambiar_turno(Turno, NuevoTurno),
+      minimax(NewDepth, NuevoTurno, NuevoTablero, NuevoScore1, NuevoScore2, _, HeuristicValue1),
+      mejor_casa(CasasRestantes, Depth, Turno, Tablero, Score1, Score2, Casa2, HeuristicValue2),
+      elegir_mejor_casa(Casa, HeuristicValue1, Casa2, HeuristicValue2, Turno, CasaElegida, HeuristicValue)
+    )
     ;
-    Valor is Score2 - Score1.
+    (
+      mejor_casa(CasasRestantes, Depth, Turno, Tablero, Score1, Score1, CasaElegida, HeuristicValue)
+    )
+  ).
+  
 
-terminal_node(Score1, Score2) :-
+elegir_mejor_casa(Casa1, HeuristicValue1, _Casa2, HeuristicValue2, Turno, Casa1, HeuristicValue1) :-
+  is_maximizing(Turno),
+  HeuristicValue1 > HeuristicValue2, !.
+
+elegir_mejor_casa(_Casa1, HeuristicValue1, Casa2, HeuristicValue2, Turno, Casa2, HeuristicValue2) :-
+  is_maximizing(Turno),
+  HeuristicValue2 > HeuristicValue1, !.
+
+elegir_mejor_casa(Casa1, HeuristicValue1, _Casa2, HeuristicValue2, _Turno, Casa1, HeuristicValue1) :-
+  HeuristicValue2 > HeuristicValue1, !.
+
+elegir_mejor_casa(_Casa1, _HeuristicValue1, Casa2, HeuristicValue2, _Turno, Casa2, HeuristicValue2).
+
+devolver_peor_val_heuristica(Turno, PeorValor) :-
+  is_maximizing(Turno),
+  PeorValor is -9999.
+
+devolver_peor_val_heuristica(_Turno, PeorValor) :-
+  PeorValor is 9999.
+
+% Siempre maximiza el jugador1
+is_maximizing(jugador1).
+
+cambiar_turno(jugador1, jugador2).
+cambiar_turno(jugador2, jugador1).
+
+
+heuristic_value(Score1, Score2, Val) :-
+  Val is Score1 - Score2.
+
+terminal_node(Score1, _Score2) :-
   Score1 > 24.
-terminal_node(Score1, Score2) :-
+terminal_node(_Score1, Score2) :-
   Score2 > 24.
 terminal_node(Score1, Score2) :-
   (Score1 is 24, Score2 is 24).
+
+
+realizar_movimiento(Casa, Tablero, Turno, Score1, Score2, NuevoScore1, NuevoScore2, NuevoTablero2) :-
+  movimiento(Casa, Tablero, Turno, NuevoTablero, CasilleroFinal),
+  recoger_semillas(CasilleroFinal, Turno, NuevoTablero, NuevoTablero2, Score1, NuevoScore1, Score2, NuevoScore2),
+  comprobar_validez(Turno,NuevoTablero2).
+
+posibles_casas(jugador1, [a1, b1, c1, d1, e1, f1]).
+posibles_casas(jugador2, [a2, b2, c2, d2, e2, f2]).
